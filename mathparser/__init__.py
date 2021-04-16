b97fd8f427465c147771b423c1fcb8463e97d76f
IAmTomahawkx <amanca@telus.net>
2021-04-15 21:14:48 -0700

    make the package pip-able

--- mathparser/__init__.py
+++ mathparser/__init__.py
@@ -2,3 +2,5 @@ from .errors import *
 from .lex import MathLexer
 from .parse import Parser
 from . import graph
+
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,42 @@
+import re
+from setuptools import setup
+
+with open("readme.md") as f:
+    readme = f.read()
+
+with open('requirements.txt') as f:
+  requirements = f.read().splitlines()
+
+with open('mathparser/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+setup(name='discord.py',
+      author='IAmTomahawkx',
+      url='https://github.com/IAmTomahawkx/math-parser',
+      project_urls={
+        "Issue tracker": "https://github.com/IAmTomahawkx/math-parser/issues",
+      },
+      version=version,
+      packages=["mathparser"],
+      license='MIT',
+      description="A math expression parser that uses AST to parse equations",
+      long_description=readme,
+      long_description_content_type="text/markdown",
+      include_package_data=True,
+      install_requires=requirements,
+      python_requires='>=3.7.0',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+      ]
+)
